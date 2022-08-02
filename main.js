3d5333cf8cf82bdeb16c6f9865fc31be2adbee0c
Tom Wilson <twilson90@hotmail.com>
2022-08-02 12:06:21 +0100

    'init'

--- main.js
+++ main.js
@@ -27,39 +27,30 @@ var color_map = {
 	"bright white": "#ffffff",
 	"metallic silver": "#acbccb",
 	"metallic gold": "#958c54",
-}
-var base_url = `./images/R51_1024x1024_2_0003_R51_1024x1024.png`;
+};
+var base_url = `./images/R51_1024x1024.png`;
 var option_map = {
 	"Branches & Birds Colour": 	{url:"./images/R51_1024x1024_2_0002_Layer-1.png", default:"basalt grey"},
 	"Flowers Colour": 			{url:"./images/R51_1024x1024_2_0001_Layer-2.png", default:"light orange"},
 	"Flowers 2 Colour": 		{url:"./images/R51_1024x1024_2_0000_Layer-3.png", default:"orange"},
+};
+
+
+const $ = (html)=>{
+	var e = document.createElement('div');
+	e.innerHTML = html;
+	return [...e.childNodes];
 }
 
 // RENDERER
-
 (async()=>{
-
-	var images = {};
-	await Promise.all([base_url, ...Object.values(option_map).map(({url})=>url)].map((url)=>{
-		return new Promise((resolve)=>{
-			var img = new Image();
-			img.src = url;
-			// img.crossOrigin = "anonymous";
-			img.onload = resolve;
-			images[url] = img;
-		});
-	}));
 
 	function capitalize(s) {
 		return s.trim().toLowerCase().replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
 	}
 
-	var main = document.querySelector(".main");
 	var img_container = document.querySelector(".img_container");
-	var loading = document.querySelector(".loading");
 	var option_container = document.querySelector(".option_container");
-	main.style.display="";
-	loading.remove()
 
 	for (var k in option_map) {
 		var label = $(`<label>${k}: </label>`)[0];
@@ -75,33 +66,50 @@ var option_map = {
 			render();
 		})
 	}
+	
+	var images = {};
+	var loading = Promise.all([base_url, ...Object.values(option_map).map(({url})=>url)].map((url)=>{
+		return new Promise((resolve)=>{
+			var img = new Image();
+			img.src = url;
+			// img.crossOrigin = "anonymous";
+			img.onload = resolve; // setTimeout(resolve,1000);
+			images[url] = img;
+		});
+	}));
+	await loading;
 
 	/** @type {HTMLCanvasElement} */
 	var canvas = $(`<canvas>`)[0];
 	canvas.width = images[base_url].width;
 	canvas.height = images[base_url].height;
-	var ctx = canvas.getContext('2d');
-	img_container.append(canvas);
 	
-	var buffer = $(`<canvas>`)[0];
-	buffer.width = canvas.width;
-	buffer.height = canvas.height;
+	var buffer = canvas.cloneNode(true);
 	var buffer_ctx = buffer.getContext('2d');
 
-	function render() {
+	async function render() {
+		await loading;
+		var old_canvas = canvas;
+		canvas = canvas.cloneNode(true);
+		img_container.append(canvas);
+		var ctx = canvas.getContext('2d');
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		ctx.drawImage(images[base_url], 0, 0);
-
+		// ctx.globalAlpha = 0.95
 		for (var k in option_map) {
 			var select = document.getElementById(k);
-
 			buffer_ctx.globalCompositeOperation = "copy";
 			buffer_ctx.drawImage(images[option_map[k].url], 0, 0);
 			buffer_ctx.globalCompositeOperation = "source-in";
 			buffer_ctx.fillStyle = color_map[select.value];
 			buffer_ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-			ctx.drawImage(buffer, 0 ,0);
+			ctx.drawImage(buffer, 0, 0);
+		}
+		if (old_canvas) {
+			canvas.addEventListener('animationend', ()=>old_canvas.remove());
+			setTimeout(()=>{
+				old_canvas.remove(); // backup
+			}, 1000);
 		}
 	}
 	render();
